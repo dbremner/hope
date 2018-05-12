2a72d9e50342d12134ec4b8ec6770ca4bd102fdb
David Bremner <dbremner@gmail.com>
2018-05-12 15:44:00 -0700

    always use 8-byte alignment instead of screwy code

--- src/align.h
+++ src/align.h
@@ -5,11 +5,6 @@
  *	Determine the alignment of doubles on the current machine.
  */
 
-typedef struct { char c; double d; } PtrRec;
-
-#define ALIGNMENT ((int)&(((PtrRec *)0)->d))
-
-/* If that doesn't work, the following is conservative: */
-/* #define ALIGNMENT sizeof(double) */
+#define ALIGNMENT sizeof(double) 
 
 #endif
